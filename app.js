83b9fc5e87633edacff3996c261ceb5bdbe180c8
alannuarii <alannuariii@gmail.com>
2024-01-05 08:09:56 +0800

    Update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { Kafka, Partitioners } = require('kafkajs');
 
 const kafka = new Kafka({
-    brokers: ['127.0.0.1:9092']
+    brokers: ['localhost:9092']
 });
 
 const producer = kafka.producer({
