2654bdedd5fd48f0031cbe47dc52dc289810e5d3
alannuarii <alannuariii@gmail.com>
2024-01-05 08:54:55 +0800

    Update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { Kafka, Partitioners } = require('kafkajs');
 
 const kafka = new Kafka({
     clientId: 'my-kafka-app', // Nama identifikasi unik untuk aplikasi Anda
-    brokers: ['localhost:9092'] // Sesuaikan dengan alamat dan port broker Kafka Anda
+    brokers: ['kafka:9092'] // Sesuaikan dengan alamat dan port broker Kafka Anda
 });
 
 const producer = kafka.producer({
